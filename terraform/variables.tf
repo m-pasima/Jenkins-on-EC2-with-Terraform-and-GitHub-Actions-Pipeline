3e05b7743294b2845abd98be4f39fb0b6b6d8928
m-pasima <theivykierachronicles@gmail.com>
2024-06-16 03:53:37 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,7 @@
 variable "key_name" {
   description = "The name of the SSH key pair"
   type        = string
+  default     = "jenkins"
 }
 
 variable "region" {
@@ -8,3 +9,4 @@ variable "region" {
   type        = string
   default     = "eu-west-2"
 }
+
