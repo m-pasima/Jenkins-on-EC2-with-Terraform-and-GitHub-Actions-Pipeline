7460fb23bad54b04ff3b37781a78527b51faf272
m-pasima <theivykierachronicles@gmail.com>
2024-06-16 04:13:56 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,14 +10,41 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main_igw"
+  }
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "main_route_table"
+  }
+}
+
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "${var.region}a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "main_subnet"
   }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
 }
 
 resource "aws_security_group" "allow_ssh_and_jenkins" {
@@ -55,6 +82,7 @@ resource "aws_instance" "jenkins_server" {
   key_name                = var.key_name
   subnet_id               = aws_subnet.main.id
   vpc_security_group_ids  = [aws_security_group.allow_ssh_and_jenkins.id]
+  associate_public_ip_address = true
 
   tags = {
     Name = "JenkinsServer"
